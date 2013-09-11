e7b51b5b061fbd494ae9e64ebfb1cec0faf97e7f
Roberto Peng <robertopeng@gmail.com>
2013-09-11 11:58:54 +0800

    add ic_sql

---
+++ sql/yb_ic_sql.sql
@@ -0,0 +1,39 @@
+/*
+SQLyog Enterprise v10.42 
+MySQL - 5.5.32 : Database - yunbian
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`yunbian` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `yunbian`;
+
+/*Table structure for table `th_invite_comment` */
+
+DROP TABLE IF EXISTS `th_invite_comment`;
+
+CREATE TABLE `th_invite_comment` (
+  `id` mediumint(8) unsigned NOT NULL,
+  `uid` int(10) unsigned NOT NULL,
+  `touid` int(10) NOT NULL,
+  `isnew` tinyint(1) unsigned NOT NULL,
+  `num` int(10) unsigned NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `status` int(10) NOT NULL,
+  `time` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
